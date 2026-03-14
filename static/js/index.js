6dc2851c9dc3d9beabdbc2e5d198c2e48c534a5b
Gabriel <gabriel.f.lima21@aluno.senai.br>
2026-03-14 15:59:13 -0300

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -6,3 +6,22 @@ function mostrarPlanos() {
         x.style.display = "block";
     }
 }
+const fadeElements = document.querySelectorAll('.fade-element');
+
+    // Cria um 'Observador' que verifica quando os elementos entram na tela
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            // Se o elemento estiver visível na tela
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible'); // Adiciona a classe que faz aparecer
+                observer.unobserve(entry.target); // Para de observar depois que já apareceu (anima só 1 vez)
+            }
+        });
+    }, {
+        threshold: 0.15 // O elemento começa a aparecer quando 15% dele está na tela
+    });
+
+    // Manda o observador olhar cada um dos elementos
+    fadeElements.forEach(element => {
+        observer.observe(element);
+    });
